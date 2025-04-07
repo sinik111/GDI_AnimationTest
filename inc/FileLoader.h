d130795ed22628984916b2af251ffaa9e42ab8b8
sinik123 <kangmark95@naver.com>
2025-04-08 00:59:40 +0900

    00

--- inc/FileLoader.h
+++ inc/FileLoader.h
@@ -1,19 +1,33 @@
 #pragma once
 
-typedef struct StringData
+class FileLoader
 {
-	int m;
-	int n;
-	int count;
-	int additional1;
-	int additional2;
-	int additional3;
-	wchar_t* data;
-} StringData;
+public:
+	static FileLoader& GetInstance();
+	void Initialize();
+private:
+	FileLoader();
+
+	FileLoader(const FileLoader&) = delete;
+	FileLoader& operator=(const FileLoader&) = delete;
+};
 
 
 
-
-StringData* LoadSingleLineData(const wchar_t* file_name);
-
-StringData* LoadMultiLineData(const wchar_t* file_name);
+//typedef struct StringData
+//{
+//	int m;
+//	int n;
+//	int count;
+//	int additional1;
+//	int additional2;
+//	int additional3;
+//	wchar_t* data;
+//} StringData;
+//
+//
+//
+//
+//StringData* LoadSingleLineData(const wchar_t* file_name);
+//
+//StringData* LoadMultiLineData(const wchar_t* file_name);
